3a4329d1989525e04b9eff707cbd01bc632342f8
Kwinten <kwintenjacobs@gmail.com>
2020-11-19 19:52:06 +0100

    Added camera controls commented in game.cs Added material to triangles in ObjectLoader.cs

--- Lighthouse3/ObjectLoader.cs
+++ Lighthouse3/ObjectLoader.cs
@@ -5,6 +5,7 @@ using OpenTK;
 using ObjLoader.Loader.Data.VertexData;
 using Lighthouse3.Primitives;
 using ObjLoader.Loader.Data.Elements;
+using System.Collections.Generic;
 
 namespace Lighthouse3
 {
@@ -22,23 +23,35 @@ namespace Lighthouse3
         {
             LoadResult loadResult = LoadObj(path);
 
-            int amount = loadResult.Groups[0].Faces.Count;
-            Triangle[] triangles = new Triangle[amount];
-            for (int i = 0; i < amount; i++)
-            {
 
-                Face face = loadResult.Groups[0].Faces[i];
-                Vector3 point1 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[0].VertexIndex - 1]);
-                Vector3 point2 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[1].VertexIndex - 1]);
-                Vector3 point3 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[2].VertexIndex - 1]);
-                triangles[i] = new Triangle(point1, point2, point3, Material.Red);
+            List<Triangle> triangles = new List<Triangle>();
+            for (int i = 0; i < loadResult.Groups.Count; i++)
+            {
+                Console.WriteLine(loadResult.Groups[i].Faces.Count);
+                int amount = loadResult.Groups[i].Faces.Count;
+                for (int j = 0; j < amount; j++)
+                {
+
+                    Face face = loadResult.Groups[i].Faces[j];
+                    Vector3 point1 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[0].VertexIndex - 1]);
+                    Vector3 point2 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[1].VertexIndex - 1]);
+                    Vector3 point3 = ObjectLoader.VertexToVector3(loadResult.Vertices[face[2].VertexIndex - 1]);
+                    triangles.Add(new Triangle(point1, point2, point3, MtlToMaterial(loadResult.Groups[i].Material)));
+                }
             }
-            return triangles;
+            
+            return triangles.ToArray();
         }
 
         public static Vector3 VertexToVector3(Vertex vertex)
         {
             return new Vector3(vertex.X, vertex.Y, vertex.Z);
+        }
+
+        private static Material MtlToMaterial(ObjLoader.Loader.Data.Material material)
+        {
+            Console.WriteLine(material.DiffuseColor.X);
+            return new Material(material.DiffuseColor.X, material.DiffuseColor.Y, material.DiffuseColor.Z, 0);
         }
     }
 }
